794313c6537fa334b854dc3d2750c7fc4492d284
stedwar2 <stedwar2>
2007-05-08 04:40:18 +0000

    Updated to support Wonder v3.0.

---
+++ EOJDBCPrototypes/src/net/sf/webcat/EOJDBCPrototypes.java
@@ -0,0 +1,41 @@
+/*==========================================================================*\
+ |  $Id: EOJDBCPrototypes.java,v 1.1 2007/05/08 04:40:18 stedwar2 Exp $
+ |*-------------------------------------------------------------------------*|
+ |  Copyright (C) 2006 Virginia Tech
+ |
+ |  This file is part of Web-CAT.
+ |
+ |  Web-CAT is free software; you can redistribute it and/or modify
+ |  it under the terms of the GNU General Public License as published by
+ |  the Free Software Foundation; either version 2 of the License, or
+ |  (at your option) any later version.
+ |
+ |  Web-CAT is distributed in the hope that it will be useful,
+ |  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ |  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ |  GNU General Public License for more details. 
+ |
+ |  You should have received a copy of the GNU General Public License
+ |  along with Web-CAT; if not, write to the Free Software
+ |  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ |
+ |  Project manager: Stephen Edwards <edwards@cs.vt.edu>
+ |  Virginia Tech CS Dept, 660 McBryde Hall (0106), Blacksburg, VA 24061 USA
+\*==========================================================================*/
+
+package net.sf.webcat;
+
+//-------------------------------------------------------------------------
+/**
+ *  This a completely empty class that simply ensures that the
+ *  EOJDBCPrototypes framework has a jar file created for it, and its
+ *  resources end up being included as a live framework by various parts
+ *  of WebObjects.
+ *
+ *  @author  stedwar2
+ *  @version $Id: EOJDBCPrototypes.java,v 1.1 2007/05/08 04:40:18 stedwar2 Exp $
+ */
+public class EOJDBCPrototypes
+{
+    // Nothing at all needed here
+}
