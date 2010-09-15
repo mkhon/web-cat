c80385129d25e3f9a9ede1c8593a1d391360d310
aallowat <aallowat>
2010-09-15 18:37:36 +0000

    Added stub classes for WCServletAdaptor and Application with the old package names for compatibility reasons.

---
+++ Bootstrap/src/net/sf/webcat/WCServletAdaptor.java
@@ -0,0 +1,48 @@
+/*==========================================================================*\
+ |  $Id: WCServletAdaptor.java,v 1.15 2010/09/15 18:37:36 aallowat Exp $
+ |*-------------------------------------------------------------------------*|
+ |  Copyright (C) 2006-2008 Virginia Tech
+ |
+ |  This file is part of Web-CAT.
+ |
+ |  Web-CAT is free software; you can redistribute it and/or modify
+ |  it under the terms of the GNU Affero General Public License as published
+ |  by the Free Software Foundation; either version 3 of the License, or
+ |  (at your option) any later version.
+ |
+ |  Web-CAT is distributed in the hope that it will be useful,
+ |  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ |  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ |  GNU General Public License for more details.
+ |
+ |  You should have received a copy of the GNU Affero General Public License
+ |  along with Web-CAT; if not, see <http://www.gnu.org/licenses/>.
+\*==========================================================================*/
+
+package net.sf.webcat;
+
+import javax.servlet.ServletException;
+
+//-------------------------------------------------------------------------
+/**
+ *  A stub with the old package name for compatibility reasons.
+ *
+ *  @author  Tony Allevato
+ *  @version $Id: WCServletAdaptor.java,v 1.15 2010/09/15 18:37:36 aallowat Exp $
+ */
+@Deprecated
+public class WCServletAdaptor extends org.webcat.WCServletAdaptor
+{
+    //~ Constructors ..........................................................
+
+    // ----------------------------------------------------------
+    /**
+     * Constructor.
+     *
+     * @throws ServletException
+     */
+    public WCServletAdaptor() throws ServletException
+    {
+        super();
+    }
+}
