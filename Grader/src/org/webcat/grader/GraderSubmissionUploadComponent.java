ef0e911528eb131c3b3d360354e85bbe4790c756
stedwar2 <stedwar2>
2010-09-26 18:06:04 +0000

    Fixed some warnings.

--- Grader/src/org/webcat/grader/GraderSubmissionUploadComponent.java
+++ Grader/src/org/webcat/grader/GraderSubmissionUploadComponent.java
@@ -1,5 +1,5 @@
 /*==========================================================================*\
- |  $Id: GraderSubmissionUploadComponent.java,v 1.3 2010/09/26 16:49:43 stedwar2 Exp $
+ |  $Id: GraderSubmissionUploadComponent.java,v 1.4 2010/09/26 18:06:04 stedwar2 Exp $
  |*-------------------------------------------------------------------------*|
  |  Copyright (C) 2006-2008 Virginia Tech
  |
@@ -22,8 +22,6 @@
 package org.webcat.grader;
 
 import com.webobjects.appserver.*;
-import com.webobjects.eoaccess.*;
-import com.webobjects.eocontrol.EOEditingContext;
 import com.webobjects.foundation.*;
 import java.io.*;
 import org.apache.log4j.Logger;
@@ -36,7 +34,8 @@ import org.webcat.core.messaging.UnexpectedExceptionMessage;
  *  {@link SubmissionInProcess} state object.
  *
  *  @author  Stephen Edwards
- *  @version $Id: GraderSubmissionUploadComponent.java,v 1.3 2010/09/26 16:49:43 stedwar2 Exp $
+ *  @author Last changed by $Author: stedwar2 $
+ *  @version $Revision: 1.4 $, $Date: 2010/09/26 18:06:04 $
  */
 public class GraderSubmissionUploadComponent
     extends GraderAssignmentComponent
@@ -183,19 +182,14 @@ public class GraderSubmissionUploadComponent
         // Clear out older jobs
         try
         {
-            NSArray oldJobs = EOUtilities.objectsMatchingValues(
-                    localContext(),
-                    EnqueuedJob.ENTITY_NAME,
-                    new NSDictionary(
-                        new Object[] {  user(),
-                                        submission.assignmentOffering()    },
-                        new Object[] { EnqueuedJob.USER_KEY,
-                                       EnqueuedJob.ASSIGNMENT_OFFERING_KEY }
-                ) );
-            for ( int i = 0; i < oldJobs.count(); i++ )
+            NSArray<EnqueuedJob> oldJobs =
+                EnqueuedJob.objectsMatchingQualifier(localContext(),
+                    EnqueuedJob.submission.dot(Submission.user).eq(user()).and(
+                    EnqueuedJob.submission.dot(Submission.assignmentOffering)
+                        .eq(submission.assignmentOffering())));
+            for (EnqueuedJob job : oldJobs)
             {
-                EnqueuedJob job = (EnqueuedJob)oldJobs.objectAtIndex( i );
-                job.setDiscarded( true );
+                job.setDiscarded(true);
             }
         }
         catch ( Exception e )
