6a4276e51e83c51a4c9fc50003d4349e6fc91747
stedwar2 <stedwar2>
2012-01-05 19:53:00 +0000

    Fixed a warning and added the license comment.

--- Grader/src/org/webcat/grader/GradingPluginResourceHandler.java
+++ Grader/src/org/webcat/grader/GradingPluginResourceHandler.java
@@ -1,9 +1,29 @@
+/*==========================================================================*\
+ |  $Id: GradingPluginResourceHandler.java,v 1.3 2012/01/05 19:53:00 stedwar2 Exp $
+ |*-------------------------------------------------------------------------*|
+ |  Copyright (C) 2011-2012 Virginia Tech
+ |
+ |  This file is part of Web-CAT.
+ |
+ |  Web-CAT is free software; you can redistribute it and/or modify
+ |  it under the terms of the GNU Affero General Public License as published
+ |  by the Free Software Foundation; either version 3 of the License, or
+ |  (at your option) any later version.
+ |
+ |  Web-CAT is distributed in the hope that it will be useful,
+ |  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ |  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ |  GNU General Public License for more details.
+ |
+ |  You should have received a copy of the GNU Affero General Public License
+ |  along with Web-CAT; if not, see <http://www.gnu.org/licenses/>.
+\*==========================================================================*/
+
 package org.webcat.grader;
 
 import java.io.File;
 import org.webcat.core.EntityResourceHandler;
 import com.webobjects.eocontrol.EOFetchSpecification;
-import er.extensions.eof.ERXQ;
 
 //-------------------------------------------------------------------------
 /**
@@ -11,8 +31,8 @@ import er.extensions.eof.ERXQ;
  * BatchResult entities through direct URLs.
  *
  * @author  Tony Allevato
- * @author  Last changed by $Author: aallowat $
- * @version $Revision: 1.2 $, $Date: 2011/03/07 16:11:14 $
+ * @author  Last changed by $Author: stedwar2 $
+ * @version $Revision: 1.3 $, $Date: 2012/01/05 19:53:00 $
  */
 public class GradingPluginResourceHandler
     extends EntityResourceHandler<GradingPlugin>
