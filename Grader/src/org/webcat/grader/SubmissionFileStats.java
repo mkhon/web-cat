f032ebd901b2332c9975a22532f7ee366176f39a
stedwar2 <stedwar2>
2012-05-25 19:12:38 +0000

    Oops--CSS improvements can't (yet) remove the css link for generated file contents blobs, since they are rendered in an iframe in the current (soon to be replaced) grading/markup page and still need their own css link tag as a result.

--- Grader/src/org/webcat/grader/SubmissionFileStats.java
+++ Grader/src/org/webcat/grader/SubmissionFileStats.java
@@ -1,5 +1,5 @@
 /*==========================================================================*\
- |  $Id: SubmissionFileStats.java,v 1.7 2012/05/17 18:17:04 stedwar2 Exp $
+ |  $Id: SubmissionFileStats.java,v 1.8 2012/05/25 19:12:38 stedwar2 Exp $
  |*-------------------------------------------------------------------------*|
  |  Copyright (C) 2006-2012 Virginia Tech
  |
@@ -43,7 +43,7 @@ import org.webcat.woextensions.WCResourceManager;
  *
  *  @author  Stephen Edwards
  *  @author  Last changed by $Author: stedwar2 $
- *  @version $Revision: 1.7 $, $Date: 2012/05/17 18:17:04 $
+ *  @version $Revision: 1.8 $, $Date: 2012/05/25 19:12:38 $
  */
 public class SubmissionFileStats
     extends _SubmissionFileStats
@@ -488,6 +488,12 @@ public class SubmissionFileStats
 
         //make the html file
         StringBuffer contents = new StringBuffer( (int)file.length() );
+
+        contents.append(
+            "<link rel=\"stylesheet\" type=\"text/css\" href=\"" );
+        contents.append(WCResourceManager.versionlessResourceURLFor(
+            "theme/base/code.css", "Core", null, request ));
+        contents.append( "\"/>\n" );
 
         //get the array of file comments from the database
         NSArray<SubmissionFileComment> myComments = comments()
