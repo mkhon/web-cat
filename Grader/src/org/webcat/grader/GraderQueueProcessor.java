de0da29bd0fced430bae4a26c5ec87039fc006a9
stedwar2 <stedwar2>
2013-08-27 02:04:01 +0000

    Fixed an extremely rare NumberFormatException problem.

--- Grader/src/org/webcat/grader/GraderQueueProcessor.java
+++ Grader/src/org/webcat/grader/GraderQueueProcessor.java
@@ -1,5 +1,5 @@
 /*==========================================================================*\
- |  $Id: GraderQueueProcessor.java,v 1.20 2013/08/11 02:09:04 stedwar2 Exp $
+ |  $Id: GraderQueueProcessor.java,v 1.21 2013/08/27 02:04:01 stedwar2 Exp $
  |*-------------------------------------------------------------------------*|
  |  Copyright (C) 2006-2012 Virginia Tech
  |
@@ -61,7 +61,7 @@ import er.extensions.eof.ERXConstant;
  *
  * @author  Amit Kulkarni
  * @author  Last changed by $Author: stedwar2 $
- * @version $Revision: 1.20 $, $Date: 2013/08/11 02:09:04 $
+ * @version $Revision: 1.21 $, $Date: 2013/08/27 02:04:01 $
  */
 public class GraderQueueProcessor
     extends Thread
@@ -1111,12 +1111,12 @@ public class GraderQueueProcessor
             String attr = properties.getProperty(attributeBase + "loc");
             if (attr != null)
             {
-                stats.setLocRaw(ERXConstant.integerForString(attr));
+                stats.setLocRaw(integerForString(attr));
             }
             attr = properties.getProperty(attributeBase + "ncloc");
             if (attr != null)
             {
-                stats.setNclocRaw(ERXConstant.integerForString(attr));
+                stats.setNclocRaw(integerForString(attr));
             }
             attr = properties.getProperty(attributeBase + "deductions");
             if (attr != null)
@@ -1126,56 +1126,67 @@ public class GraderQueueProcessor
             attr = properties.getProperty(attributeBase + "remarks");
             if (attr != null)
             {
-                stats.setRemarksRaw(ERXConstant.integerForString(attr));
+                stats.setRemarksRaw(integerForString(attr));
             }
             attr = properties.getProperty(attributeBase + "conditionals");
             if (attr != null)
             {
-                stats.setConditionalsRaw(ERXConstant.integerForString(attr));
+                stats.setConditionalsRaw(integerForString(attr));
             }
             attr = properties.getProperty(
                 attributeBase + "conditionalsCovered");
             if (attr != null)
             {
-                stats.setConditionalsCoveredRaw(
-                    ERXConstant.integerForString(attr));
+                stats.setConditionalsCoveredRaw(integerForString(attr));
             }
             attr = properties.getProperty(attributeBase + "statements");
             if (attr != null)
             {
-                stats.setStatementsRaw(ERXConstant.integerForString(attr));
+                stats.setStatementsRaw(integerForString(attr));
             }
             attr = properties.getProperty(
                 attributeBase + "statementsCovered");
             if (attr != null)
             {
-                stats.setStatementsCoveredRaw(
-                    ERXConstant.integerForString(attr));
+                stats.setStatementsCoveredRaw(integerForString(attr));
             }
             attr = properties.getProperty(attributeBase + "methods");
             if (attr != null)
             {
-                stats.setMethodsRaw(ERXConstant.integerForString(attr));
+                stats.setMethodsRaw(integerForString(attr));
             }
             attr = properties.getProperty(
                 attributeBase + "methodsCovered");
             if (attr != null)
             {
-                stats.setMethodsCoveredRaw(
-                    ERXConstant.integerForString(attr));
+                stats.setMethodsCoveredRaw(integerForString(attr));
             }
             attr = properties.getProperty(attributeBase + "elements");
             if (attr != null)
             {
-                stats.setElementsRaw(ERXConstant.integerForString(attr));
+                stats.setElementsRaw(integerForString(attr));
             }
             attr = properties.getProperty(
                 attributeBase + "elementsCovered");
             if (attr != null)
             {
-                stats.setElementsCoveredRaw(ERXConstant.integerForString(attr));
+                stats.setElementsCoveredRaw(integerForString(attr));
             }
             stats.setSubmissionResultRelationship(submissionResult);
+        }
+    }
+
+
+    // ----------------------------------------------------------
+    private static Integer integerForString(String value)
+    {
+        try
+        {
+            return ERXConstant.integerForString(value);
+        }
+        catch (NumberFormatException e)
+        {
+            return null;
         }
     }
 
